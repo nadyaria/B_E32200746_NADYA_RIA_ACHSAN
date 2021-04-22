2653139cd49f8b40d0e4eb7bc78e18b787c5ba94
NadyaRia <nadyariaa@gmail.com>
2021-04-22 11:13:21 +0700

    tugas minggu 7

---
+++ tugas minggu 7/Laravel_Nadya/app/Http/Controllers/nadya.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class nadya extends Controller
+{
+ public function index()
+{
+       // return "Halo ini adalah method index, dalam controller nadya";
+      // return "Method ini nantinya akan digunakan untuk mengambil semua data user"; 
+    $nama = "Nadya Ria Achsan"; 
+
+    $pelajaran = ["Algoritma & Pemrograman", "Kalkulus" , "Pemrograman Web"];
+
+    return view('Ria', compact('nama', 'pelajaran'));
+}
+public function create()
+{
+    return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user";
+}
+public function store(Request $request){
+    return "Method ini nantinya akan digunakan untuk menciptakan data user yang baru";
+}
+public function show($id){
+    return "Method ini nantinya akan digunakan untuk mengambil satu data user dengan id-". $id;
+}
+public function edit($id){
+    return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id-";
+}
+public function update(Request $request, $id){
+    return "Method ini nantinya akan digunakan untuk mengubah data user dengan id-". $id;
+}
+public function destroy($id){
+    return "Method ini nantinya akan digunakan untuk menghapus data user dengan id-". $id;
+}
+}
